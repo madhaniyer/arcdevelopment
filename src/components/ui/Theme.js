6b068d79321580f4c3620714ef66c378280f0f25
Madhan Vasudevan <madhaniyer@gmail.com>
2021-08-22 17:02:21 +1200

    doc floc

---
+++ src/components/ui/Theme.js
@@ -0,0 +1,35 @@
+import { createTheme } from '@material-ui/core/styles';
+const arcBlue = "#0B72B9";
+const arcGreen = "#194035";
+const arecOrange = "#FFBA60";
+
+export default createTheme({
+    palette : {
+        common : {
+            arcBlue: `${arcBlue}`,
+            arcOrange: `${arecOrange}`,
+            arcGreen: `${arcGreen}`,
+
+        },
+        primary: {
+            main: `${arcBlue}`
+        },
+        secondary: {
+            main: `${arecOrange}`
+        }
+    },
+    typography : {
+        tab: {
+            fontSize: "1rem",
+            textTransform: "none",
+            fontWeight: 700,
+            fontFamily: "Raleway",
+        },
+        estimate: {
+            fontFamily: "Pacifico",
+            fontSize: "1rem",
+            textTransform: "none",
+            color: "purple"
+        }
+    }
+})
